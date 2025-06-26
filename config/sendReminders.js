c94e09f87b7b6f47cc8c9d5300b3ac301683618e
SaumyaSingh04 <saumya0419@gmail.com>
2025-06-26 14:59:50 +0530

    set time

--- config/sendReminders.js
+++ config/sendReminders.js
@@ -70,7 +70,7 @@ const runReminderLogic = async () => {
 };
 
 // 🕐 Cron job scheduled at 14:51 IST daily
-cron.schedule("59 14 * * *", async () => {
+cron.schedule("04 15 * * *", async () => {
   console.log("⏰ Cron running at:", new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }));
   try {
     await runReminderLogic();

b91d99ca5c0c45ba0c4e0fbeb51cdeba5188085d
SaumyaSingh04 <saumya0419@gmail.com>
2025-06-27 17:05:02 +0530

    add api

---
+++ api/cron.js
@@ -0,0 +1,12 @@
+import { runReminderForAllLeads } from "../utils/reminderLogic.js";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") return res.status(405).end("Method Not Allowed");
+
+  try {
+    await runReminderForAllLeads();
+    return res.status(200).json({ message: "Reminder cron executed" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}

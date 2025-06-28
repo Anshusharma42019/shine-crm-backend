df254faa3242de190adef00ae0fae9ea14a47c2c
SaumyaSingh04 <saumya0419@gmail.com>
2025-06-28 12:34:15 +0530

    update vercel

--- api/cron.js
+++ api/cron.js
@@ -5,10 +5,12 @@ export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end("Method Not Allowed");
 
   try {
+    console.log("🔁 Cron triggered");
     await connectDB(); 
     await runReminderForAllLeads();
     return res.status(200).json({ message: "Reminder cron executed" });
   } catch (error) {
+    console.error("❌ Cron error:", error.message);
     return res.status(500).json({ error: error.message });
   }
 }

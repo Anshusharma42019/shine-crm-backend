bb894f99be61c2ba735b8b5930de2f16cd538c9f
SaumyaSingh04 <saumya0419@gmail.com>
2025-06-28 12:54:02 +0530

    change cron

--- api/cron-Job.js
+++ api/cron-Job.js
@@ -2,12 +2,14 @@ import connectDB from "../config/db.js";
 import { runReminderForAllLeads } from "../utils/reminderLogic.js";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") return res.status(405).end("Method Not Allowed");
+  if (req.method !== "GET") return res.status(405).send("Method Not Allowed");
 
-  console.log("✅ Cron Job Triggered");
+  console.log("✅ Cron job triggered");
+
   try {
     await connectDB();
     await runReminderForAllLeads();
+    console.log("✅ Reminder logic executed");
     return res.status(200).json({ message: "Reminder cron executed" });
   } catch (error) {
     console.error("❌ Cron Job Error:", error.message);

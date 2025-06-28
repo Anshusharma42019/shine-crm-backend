85402937b7548b20aa0978c06fffa98673fabfc6
SaumyaSingh04 <saumya0419@gmail.com>
2025-06-28 12:50:20 +0530

    set time

--- api/cron-Job.js
+++ api/cron-Job.js
@@ -4,13 +4,13 @@ import { runReminderForAllLeads } from "../utils/reminderLogic.js";
 export default async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end("Method Not Allowed");
 
+  console.log("✅ Cron Job Triggered");
   try {
-    console.log("🔁 Cron triggered");
-    await connectDB(); 
+    await connectDB();
     await runReminderForAllLeads();
     return res.status(200).json({ message: "Reminder cron executed" });
   } catch (error) {
-    console.error("❌ Cron error:", error.message);
+    console.error("❌ Cron Job Error:", error.message);
     return res.status(500).json({ error: error.message });
   }
 }
